2d5bdfde7e84fd5558dccc6895732f9f3a7699ad
IPvZero <46572101+IPvZero@users.noreply.github.com>
2023-03-02 12:14:40 +0000

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,5 +1,11 @@
 from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
 
 nr = InitNornir(config_file="config.yaml")
 
-print(nr.inventory.hosts)
+def test_connect(task):
+  task.run(task=netmiko_send_command, command_string="show configuration")
+ 
+results = nr.run(task=test_connect)
+print_result(results)
