e80216a0da261e86f40ef0328c6d3f4f2e495df2
IPvZero <46572101+IPvZero@users.noreply.github.com>
2023-03-06 10:32:20 +0000

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,3 +1,4 @@
+#test
 from nornir import InitNornir
 from nornir_netmiko.tasks import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
@@ -5,7 +6,7 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="config.yaml")
 
 def test_connect(task):
-  task.run(task=netmiko_send_command, command_string="show configuration")
+  task.run(task=netmiko_send_command, command_string="show configuration | display xml")
  
 results = nr.run(task=test_connect, name="TEST SCRIPT AGAIN!")
 print_result(results)
