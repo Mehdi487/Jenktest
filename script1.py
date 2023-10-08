771ba4a8b33828101bd4689e7f35fb1baf9fe384
Mehdi487 <53235410+Mehdi487@users.noreply.github.com>
2023-10-09 00:26:46 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -8,8 +8,8 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="config.yaml")
 
 def test_connect(task):
-#  task.run(task=netmiko_send_command, command_string="show connfiguration | display xml")
-  task.run(task=netmiko_send_command, command_string="show connfiguration")
+#  task.run(task=netmiko_send_command, command_string="show connfiguration | display xml") < This should be "configuration" in main code
+  task.run(task=netmiko_send_command, command_string="show configuration")
  
 results = nr.run(task=test_connect, name="TEST SCRIPT AGAIN!")
 print_result(results)
