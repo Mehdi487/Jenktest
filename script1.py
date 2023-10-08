621a21773be9764100e870467cf32c6fef1bbdbc
Mehdi487 <53235410+Mehdi487@users.noreply.github.com>
2023-10-09 00:22:13 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -9,7 +9,7 @@ nr = InitNornir(config_file="config.yaml")
 
 def test_connect(task):
 #  task.run(task=netmiko_send_command, command_string="show connfiguration | display xml")
-  task.run(task=netmiko_send_command, command_string="show configuration", use_genie=True)
+  task.run(task=netmiko_send_command, command_string="show configuration")
  
 results = nr.run(task=test_connect, name="TEST SCRIPT AGAIN!")
 print_result(results)
